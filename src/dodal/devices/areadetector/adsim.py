6c9cfb00897870e87d433ae911519565e26120f6
Callum Forrester <callum.forrester@diamond.ac.uk>
2023-06-23 15:43:28 +0100

    Make simulated detector prime plugins

--- src/dodal/devices/areadetector/adsim.py
+++ src/dodal/devices/areadetector/adsim.py
@@ -2,7 +2,12 @@ from ophyd import Component as Cpt
 from ophyd.areadetector.base import ADComponent as Cpt
 from ophyd.areadetector.detectors import DetectorBase
 
-from .adutils import Hdf5Writer, SingleTriggerV33, SynchronisedAdDriverBase
+from .adutils import (
+    Hdf5Writer,
+    PluginWiring,
+    SingleTriggerV33,
+    SynchronisedAdDriverBase,
+)
 
 
 class AdSimDetector(SingleTriggerV33, DetectorBase):
@@ -17,8 +22,9 @@ class AdSimDetector(SingleTriggerV33, DetectorBase):
         lazy=True,
     )
 
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(self, *args, wiring: PluginWiring, **kwargs) -> None:
         super().__init__(*args, **kwargs)
+        self.plugin_wiring = wiring
         self.hdf.kind = "normal"
 
         self.stage_sigs = {
@@ -46,5 +52,41 @@ class AdSimDetector(SingleTriggerV33, DetectorBase):
             acquire_time = self.cam.acquire_time.get()
         self.stage_sigs[self.cam.acquire_period] = acquire_time
 
+        # Ensure plugins are primed
+        self._minimal_frame(timeout=10.0)
+
         # Now calling the super method should set the acquire period
         super(AdSimDetector, self).stage(*args, **kwargs)
+
+    def _minimal_frame(self, timeout: float) -> None:
+        """
+        Take a single frame and pipe it through all enabled plugins
+        """
+
+        priming_settings = {
+            self.hdf.enable: 1,
+            self.hdf.nd_array_port: self.cam.port_name.get(),
+            self.cam.array_callbacks: 1,
+            self.cam.image_mode: "Single",
+            self.cam.trigger_mode: "Internal",
+            # Take the quickest possible frame
+            self.cam.acquire_time: 0.0,
+            self.cam.acquire_period: 0.0,
+        }
+
+        # Backup state and ensure we are not acquiring
+        reset_to = {signal: signal.get() for signal in priming_settings.keys()}
+        self.cam.acquire.set(0).wait(timeout=timeout)
+
+        try:
+            # Apply all settings for acquisition
+            for signal, value in priming_settings.items():
+                # Ensure that .wait really will wait until the PV is set including its RBV
+                signal.set(value).wait(timeout=timeout)
+
+            # Acquire a frame
+            self.cam.acquire.set(1).wait(timeout=timeout)
+        finally:
+            # Revert settings to previous values
+            for signal, value in reversed(reset_to.items()):
+                signal.set(value).wait(timeout=timeout)
