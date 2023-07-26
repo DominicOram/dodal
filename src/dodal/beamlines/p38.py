481f5f4a80091a263e5461f8af4c88b370e32f81
Keith Ralphs <keith.ralphs@diamond.ac.uk>
2023-07-26 11:45:43 +0100

    Remove BL declaration

--- src/dodal/beamlines/p38.py
+++ src/dodal/beamlines/p38.py
@@ -1,14 +1,8 @@
-from dodal.beamlines.beamline_utils import BL, device_instantiation
-from dodal.beamlines.beamline_utils import set_beamline as set_utils_beamline
+from dodal.beamlines.beamline_utils import device_instantiation
 from dodal.devices.areadetector import AdAravisDetector
-from dodal.log import set_beamline
-from dodal.utils import BeamlinePrefix, get_beamline_name
+from dodal.utils import BeamlinePrefix
 
 PREFIX: str = BeamlinePrefix("p38").beamline_prefix
-
-BL = get_beamline_name("p38")
-set_beamline(BL)
-set_utils_beamline(BL)
 
 
 def d11(
