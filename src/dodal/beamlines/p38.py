1d4beced1343ae7b1f1d759943d727e156fe70c4
Joseph Ware <joseph.ware@diamond.ac.uk>
2023-07-28 14:52:26 +0100

    Hardcode p38 detector h5 path to commissioning directory

--- src/dodal/beamlines/p38.py
+++ src/dodal/beamlines/p38.py
@@ -5,8 +5,12 @@ PREFIX: str = BeamlinePrefix("p38").beamline_prefix
 
 
 def d11(name: str = "D11") -> AdAravisDetector:
-    return AdAravisDetector(name=name, prefix=f"{PREFIX}-DI-DCAM-03:")
+    det = AdAravisDetector(name=name, prefix=f"{PREFIX}-DI-DCAM-03:")
+    det.hdf.reg_root = "/dls/p38/data/2023/cm33874-3"
+    return det
 
 
 def d12(name: str = "D12") -> AdAravisDetector:
-    return AdAravisDetector(name=name, prefix=f"{PREFIX}-DI-DCAM-04:")
+    det = AdAravisDetector(name=name, prefix=f"{PREFIX}-DI-DCAM-04:")
+    det.hdf.reg_root = "/dls/p38/data/2023/cm33874-3"
+    return det
