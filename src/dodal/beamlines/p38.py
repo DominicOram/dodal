1e0c29658547b0decfa8d5e452fa827c633addec
DiamondJoseph <53935796+DiamondJoseph@users.noreply.github.com>
2023-08-04 14:08:22 +0100

    Update p38.py

--- src/dodal/beamlines/p38.py
+++ src/dodal/beamlines/p38.py
@@ -7,10 +7,12 @@ PREFIX: str = BeamlinePrefix("p38").beamline_prefix
 def d11(name: str = "D11") -> AdAravisDetector:
     det = AdAravisDetector(name=name, prefix=f"{PREFIX}-DI-DCAM-03:")
     det.hdf.reg_root = "/dls/p38/data/2023/cm33874-3"
+    det.hdf.write_path_template = "."
     return det
 
 
 def d12(name: str = "D12") -> AdAravisDetector:
     det = AdAravisDetector(name=name, prefix=f"{PREFIX}-DI-DCAM-04:")
     det.hdf.reg_root = "/dls/p38/data/2023/cm33874-3"
+    det.hdf.write_path_template = "."
     return det
