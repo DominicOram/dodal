fe9bdcd61a2d5eb6b1e903a494cfbdbfb7252cd5
DiamondJoseph <53935796+DiamondJoseph@users.noreply.github.com>
2023-08-08 15:35:31 +0100

    Update p38.py
    RE: https://github.com/bluesky/ophyd/issues/1146

--- src/dodal/beamlines/p38.py
+++ src/dodal/beamlines/p38.py
@@ -8,6 +8,7 @@ def d11(name: str = "D11") -> AdAravisDetector:
     det = AdAravisDetector(name=name, prefix=f"{PREFIX}-DI-DCAM-03:")
     det.hdf.reg_root = "/dls/p38/data/2023/cm33874-3"
     det.hdf.write_path_template = "."
+    det.hdf.read_path_template = "."
     return det
 
 
@@ -15,4 +16,5 @@ def d12(name: str = "D12") -> AdAravisDetector:
     det = AdAravisDetector(name=name, prefix=f"{PREFIX}-DI-DCAM-04:")
     det.hdf.reg_root = "/dls/p38/data/2023/cm33874-3"
     det.hdf.write_path_template = "."
+    det.hdf.read_path_template = "."
     return det
